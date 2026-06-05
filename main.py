7bf852de28a9c8a59750d42662fac44e51d7eb75
CZY774 <yogaliuze@gmail.com>
2026-06-05 20:58:17 +0700

    fix: Correct syntax error in CORS middleware configuration

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ app.add_middleware(
     allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["*"],
+    allow_headers=["*"]s,
 )
 
 
